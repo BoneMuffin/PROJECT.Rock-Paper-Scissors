b5891903fb79df8c03c26aec1c87d0417f41f332
valen <valen.bagaglia@gmail.com>
2022-08-19 11:35:24 -0300

    randomized computer's choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,23 +2,39 @@
 console.log("Hello World!");
 
 // the user inputs a text //
-const userHand =( userInput) => {
+const userHand = (userInput) => {
     userInput = userInput.toLowerCase();
-    if (userInput == "rock") || userInput == "paper" || userInput == "scissors" ) {
-        
-    } 
-}
+    if (userInput == "rock" || userInput == "paper" || userInput == "scissors") {
+        return userInput;
+    } else {
+        console.log ("You should only pick between rock, paper and scissors");
+    }
+};
 //input needs to be stored inside a var //
 
 // randomize the computer's choice //
-function playRound(playerSelection, computerSelection) {
-
-}
-    // if user == computer //
+const getComputerChoice = () => {
+    constRandomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+};
+// if user == computer //
        // result = tie //
 
+const winnerIs = (userChoice, computerChoice) => {
+    
+}
     // compare results with ELSE and IF statements //
+    function playRound(playerSelection, computerSelection) {
 
+    }
+        
 // log different messages depending on results //
 
 // back to beginning for new inputs from both players //
