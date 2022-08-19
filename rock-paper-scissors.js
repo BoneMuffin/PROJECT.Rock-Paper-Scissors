caf5454deb0d047cb9a371285cb7caed1a4739c1
valen <valen.bagaglia@gmail.com>
2022-08-19 11:08:28 -0300

    started to define constant for user input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,37 @@
-function playRound(playerSelection, computerSelection) {
-    
+// text showing 'pick one' in the console //
+console.log("Hello World!");
+
+// the user inputs a text //
+const userHand =( userInput) => {
+    userInput = userInput.toLowerCase();
+    if (userInput == "rock") || userInput == "paper" || userInput == "scissors" ) {
+        
+    } 
 }
+//input needs to be stored inside a var //
+
+// randomize the computer's choice //
+function playRound(playerSelection, computerSelection) {
+
+}
+    // if user == computer //
+       // result = tie //
+
+    // compare results with ELSE and IF statements //
+
+// log different messages depending on results //
+
+// back to beginning for new inputs from both players //
+
+    // print the result //
+    // ask if they want to play again //
+    // if answer is no: //
+    // break loop //
+
+        // end //
+    // print "thank you for playing" //
+    // exit //
+
+
+// add a score //
+
