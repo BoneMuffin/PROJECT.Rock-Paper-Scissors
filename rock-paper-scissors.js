a4eb9c7f26460f15765c04ee10660d452bb4fd96
valen <valen.bagaglia@gmail.com>
2022-08-22 13:17:09 -0300

    modified console logs to work properly

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,16 @@
 let playerScore = 0
 let compScore = 0
 
+// Randomize the computer's choice STEP 3 //
+const getComputerChoice = () => {
+    const arrOfChoices = ['rock', 'paper', 'scissors']
+    const randomNum = Math.floor(Math.random() * arrOfChoices.length)
+    return arrOfChoices[randomNum]
+};
+
 // Get user input //
 const getUserChoice = (playerSelection) => {
-
+    let userInput = prompt("Choose your weapon!", "Rock, Paper, Scissors")
     if (userInput.toLowerCase() === 'rock'){
      return userInput;
      }
@@ -14,53 +21,51 @@ const getUserChoice = (playerSelection) => {
        return userInput;
      }
      else {console.log('Not Valid. Start running.')}
-     };
+     }; 
 
-// Randomize the computer's choice //
-const getComputerChoice = () => {
-    const arrOfChoices = ['rock', 'paper', 'scissors']
-    const randomNum = Math.floor(Math.random() * arrOfChoices.length)
-    return arrOfChoices[randomNum]
-};
 
- function playRound (playerSelection, computerSelection) {
+// STEP 4, single round //
+ const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) { // if user == computer //
-        return console.log('You got a tie! Go figure!');
+        return ("You got a tie! Go figure!"); 
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        compScore++; // stands for compScore = compScore + compScore //
-        return console.log('You lose! Rock beats scissors!');
+        compScore++ // stands for compScore = compScore + compScore //
+        return ("You lose! Rock beats scissors!");
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        playerScore++;
-        return console.log('You win! Scissors beat paper!');
+        playerScore++
+        return ("You win! Scissors beat paper!");
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        compScore++;
-        return console.log('You lose! Paper beats rock!');
+        compScore++
+        return ("You lose! Paper beats rock!");
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        playerScore++;
-        return console.log('You win! Rock beats scissors! Have a cookie.');
+        playerScore++
+        return ("You win! Rock beats scissors! Have a cookie.");
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        compScore++;
-        return console.log('You lose! Scissors beat paper! Activating trapdoor...');
+        compScore++
+        return ("You lose! Scissors beat paper! Activating trapdoor...");
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        playerScore++;
-        return console.log('You win! Paper beats rock!');
-    }
+        playerScore++
+        return ("You win! Paper beats rock!");
+   }
 }
 
-
-// Loop for the rounds //
+// STEP 6 Loop for the rounds //
 const game = () => {
-    for (count = 0; count <= 5; count++) {
-        const playerSelection = prompt('Choose your weapon!', 'Rock, Paper, Scissors')
-        const computerSelection = getComputerChoice();
-    }
+ for (let i = 1; i <= 5; i++) {
+    const playerSelection = prompt ('Choose your weapon!','Rock, Paper, Scissors').toLowerCase()
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+
+ }
+
      // print the result //
-     if (playerScore > compScore) {
-        return console.log('You won the game! GG!')
+ if (playerScore > compScore) {
+        return 'You won the game! GG!'
     } else if (playerScore < compScore) {
-        return console.log("You lost the game! The computer is now basking in its own victory.")
+        return 'You lost the game! The computer is now basking in its own victory.'
     } else {
-        return console.log('The game ended in a tie! Existential crisis incoming...')
-    }
-}
-game()
+        return 'The game ended in a tie! Existential crisis incoming...'
+     } 
+ }
+
+console.log(game())
